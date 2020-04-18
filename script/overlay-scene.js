1bb6d8acee170fca7b00d8f05b546557ef122cdf
Chris Rock <chris.rock@logianalytics.com>
2020-04-18 00:39:25 -0400

    Update phaser typings

---
+++ script/overlay-scene.js
@@ -0,0 +1,18 @@
+/** @type {import("../typings/phaser")} */
+
+import globalConfig from './global-config.js';
+import SystemState from './state-machine.js';
+
+class MainScene extends Phaser.Scene {
+    init() {
+        console.log("Main Scene Init");
+    }
+    create() {
+        
+    }
+    update(time, delta) {
+        
+    }
+}
+
+export default MainScene;
